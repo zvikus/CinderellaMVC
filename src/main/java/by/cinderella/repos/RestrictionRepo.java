75c6d70a056ab6755d32c5f180875f1bd8a31f5c
zvikus <cvichkovski@gmail.com>
2022-06-02 12:25:35 +0300

    Refactoring.

--- src/main/java/by/cinderella/repos/RestrictionRepo.java
+++ src/main/java/by/cinderella/repos/RestrictionRepo.java
@@ -1,7 +1,6 @@
 package by.cinderella.repos;
 
 import by.cinderella.model.user.Restriction;
-import by.cinderella.model.user.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestrictionRepo extends JpaRepository<Restriction, Long> {
--- src/main/java/by/cinderella/services/MailSender.java
+++ src/main/java/by/cinderella/services/MailSender.java
@@ -1,9 +1,6 @@
 package by.cinderella.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import javax.mail.*;
@@ -13,25 +10,32 @@ import java.util.Properties;
 
 @Service
 public class MailSender {
-    @Autowired
-    private JavaMailSender mailSender;
 
     @Value("{spring.mail.username}")
     private String username;
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.password}")
+    private String password;
+    @Value("${spring.mail.port}")
+    private int port;
+    @Value("${spring.mail.socket.factory.class}")
+    private String socketFactoryClass;
+    @Value("${spring.mail.smtp.auth}")
+    private String isSmptAuth;
 
     public  void send(String emailTo, String subject, String message) {
         Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.socketFactory.port", port);
+        props.put("mail.smtp.socketFactory.class", socketFactoryClass);
+        props.put("mail.smtp.auth", isSmptAuth);
+        props.put("mail.smtp.port", port);
         //get Session
         Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication("cinderella.minsk.service@gmail.com","lhiepbmdvdqrgwxi");
+                        return new PasswordAuthentication(username, password);
                     }
                 });
         //compose message
@@ -42,18 +46,7 @@ public class MailSender {
             mimeMessage.setText(message);
             //send message
             Transport.send(mimeMessage);
-            System.out.println("message sent successfully");
         } catch (MessagingException e) {throw new RuntimeException(e);}
 
     }
-
-
-        /*SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom(username);
-        mailMessage.setTo(emailTo);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(message);
-
-        mailSender.send(mailMessage);
-    }*/
 }
